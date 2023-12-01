3e6eb4e89429a8f0c075158237791e8722c5b824
Matheus <bertonha_2911@hotmail.com>
2023-12-01 00:38:05 -0300

    Inserção das cores, mas ainda não esta pegando cores corretas

--- ImageAttributesExtract/ColorsDictionary.cs
+++ ImageAttributesExtract/ColorsDictionary.cs
@@ -1,116 +1,23 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Drawing;
-using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace ExtrairInformacoesImagem
+namespace ImageAttributesExtract
 {
-    class Program
+    public class ColorsDictionary
     {
-        static void Main()
+        public Dictionary<string, Color> CoresPredefinidas { get; }
+
+        public ColorsDictionary()
         {
-            string diretorioImagens = @"E:\Imagens";
-            string urlImagem = @"E:\Imagens\teste.JPG";
-
-            if (Directory.Exists(diretorioImagens))
-            {
-                Bitmap imagem = new Bitmap(urlImagem);
-
-                Color corCabelo = IdentificarCorCabelo(imagem);
-                Color corOlhos = IdentificarCorOlhos(imagem);
-                Color corRoupa = IdentificarCorRoupa(imagem);
-
-                string cabelo = IdentificarNomeCor(corCabelo);
-                string olhos = IdentificarNomeCor(corOlhos);
-                string roupa = IdentificarNomeCor(corRoupa);
-
-                Console.WriteLine($"Cor do cabelo: {cabelo}");
-                Console.WriteLine($"Cor dos olhos: {olhos}");
-                Console.WriteLine($"Cor da roupa: {roupa}");
-            }
-            else
-            {
-                Console.WriteLine("O diretório de imagens especificado não existe.");
-            }
-
-            Console.WriteLine("Pressione qualquer tecla para sair.");
-            Console.ReadKey();
-        }
-
-        static Color IdentificarCorCabelo(Bitmap imagem)
-        {
-            int alturaRegiaoCabelo = imagem.Height / 4;
-
-            Rectangle retanguloRegiaoCabelo = new Rectangle(0, 0, imagem.Width, alturaRegiaoCabelo);
-            Bitmap regiaoCabelo = imagem.Clone(retanguloRegiaoCabelo, imagem.PixelFormat);
-
-            Color corPredominanteCabelo = EncontrarCorPredominante(regiaoCabelo);
-
-            return corPredominanteCabelo;
-        }
-
-        static Color IdentificarCorOlhos(Bitmap imagem)
-        {
-            int xInicioOlhos = imagem.Width / 4;
-            int yInicioOlhos = imagem.Height / 4;
-            int larguraRegiaoOlhos = imagem.Width / 2;
-            int alturaRegiaoOlhos = imagem.Height / 4;
-
-            Rectangle retanguloRegiaoOlhos = new Rectangle(xInicioOlhos, yInicioOlhos, larguraRegiaoOlhos, alturaRegiaoOlhos);
-            Bitmap regiaoOlhos = imagem.Clone(retanguloRegiaoOlhos, imagem.PixelFormat);
-
-            Color corPredominanteOlhos = EncontrarCorPredominante(regiaoOlhos);
-
-            return corPredominanteOlhos;
-        }
-
-        static Color IdentificarCorRoupa(Bitmap imagem)
-        {
-
-            int yInicioRoupa = imagem.Height / 4;
-            int alturaRegiaoRoupa = imagem.Height - yInicioRoupa;
-
-            // Extrair a região que provavelmente contém as roupas
-            Rectangle retanguloRegiaoRoupa = new Rectangle(0, yInicioRoupa, imagem.Width, alturaRegiaoRoupa);
-            Bitmap regiaoRoupa = imagem.Clone(retanguloRegiaoRoupa, imagem.PixelFormat);
-
-            // Encontrar a cor predominante na região das roupas
-            Color corPredominanteRoupa = EncontrarCorPredominante(regiaoRoupa);
-
-            return corPredominanteRoupa;
-        }
-
-        static Color EncontrarCorPredominante(Bitmap imagem)
-        {
-
-            int totalRed = 0, totalGreen = 0, totalBlue = 0;
-            int totalPixels = 0;
-
-            for (int x = 0; x < imagem.Width; x++)
-            {
-                for (int y = 0; y < imagem.Height; y++)
-                {
-                    Color pixelColor = imagem.GetPixel(x, y);
-                    totalRed += pixelColor.R;
-                    totalGreen += pixelColor.G;
-                    totalBlue += pixelColor.B;
-                    totalPixels++;
-                }
-            }
-
-            int mediaRed = totalRed / totalPixels;
-            int mediaGreen = totalGreen / totalPixels;
-            int mediaBlue = totalBlue / totalPixels;
-
-            return Color.FromArgb(mediaRed, mediaGreen, mediaBlue);
-        }
-
-        static string IdentificarNomeCor(Color cor)
-        {
-            #region Dicionario de cores
-            // Mapeamento de cores predefinidas com nomes de cores
-            Dictionary<string, Color> coresPredefinidas = new Dictionary<string, Color>
+            CoresPredefinidas = new Dictionary<string, Color>
             {
                 {"Black", Color.Black},
+                {"White", Color.White},
+                {"Red", Color.Red},
                 {"Green", Color.Green},
                 {"Blue", Color.Blue},
                 {"Coral", Color.Coral},
@@ -148,6 +55,7 @@ namespace ExtrairInformacoesImagem
                 {"Gold", Color.Gold},
                 {"Goldenrod", Color.Goldenrod},
                 {"Gray", Color.Gray},
+                {"Green", Color.Green},
                 {"GreenYellow", Color.GreenYellow},
                 {"Honeydew", Color.Honeydew},
                 {"HotPink", Color.HotPink},
@@ -238,33 +146,6 @@ namespace ExtrairInformacoesImagem
                 {"Yellow", Color.Yellow},
                 {"YellowGreen", Color.YellowGreen}
             };
-            #endregion
-
-            // Procura pela cor mais próxima do valor RGB fornecido
-            string nomeCorMaisProxima = "Desconhecida";
-            double menorDistancia = double.MaxValue;
-
-            foreach (var nomeCor in coresPredefinidas.Keys)
-            {
-                double distancia = CalcularDistancia(cor, coresPredefinidas[nomeCor]);
-                if (distancia < menorDistancia)
-                {
-                    menorDistancia = distancia;
-                    nomeCorMaisProxima = nomeCor;
-                }
-            }
-
-            return nomeCorMaisProxima;
         }
-
-        static double CalcularDistancia(Color corA, Color corB)
-        {
-            int dR = corA.R - corB.R;
-            int dG = corA.G - corB.G;
-            int dB = corA.B - corB.B;
-
-            return Math.Sqrt(dR * dR + dG * dG + dB * dB);
-        }
-
     }
 }
